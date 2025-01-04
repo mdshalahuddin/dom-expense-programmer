e2fe15bc14fd07b56311f38beffef9393c06ced7
shalahuddin <mdshalahuddin004@gmail.com>
2025-01-04 19:45:54 +0600

    input value

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const calculateButton = document.getElementById('calculate');
+
+calculateButton.addEventListener("click", function () {
+    const income = parseFloat(document.getElementById("income").value);
+    const software = parseFloat(document.getElementById("software").value);
+    const courses = parseFloat(document.getElementById("courses").value);
+    const internet = parseFloat(document.getElementById("internet").value);
+    console.log({income,software,courses,internet});
+})
